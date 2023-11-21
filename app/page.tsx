382672b9f880f2c9169d2c111c32bc38afac999d
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-11-20 21:44:39 -0500

    🧪 Some testing with Next.js added

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,9 @@ export default function IndexPage() {
           built with Radix UI and Tailwind CSS.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
+          Hablálo mi fafá
+        </p>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
           Accessible and customizable components that you can copy and paste
           into your apps. Free. Open Source. And Next.js 13 Ready.
         </p>
--- config/site.ts
+++ config/site.ts
@@ -1,18 +1,22 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "NumAnalytic Solver - Interactive Numerical Methods Tool",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "NumAnalytic Solver is an interactive tool providing efficient solutions for various numerical methods like Bisection, Newton-Raphson, Jacobi, and more. Ideal for students, educators, and researchers in numerical analysis.",
   mainNav: [
     {
       title: "Home",
       href: "/",
     },
+    {
+      title: "Hi",
+      href: "/",
+    },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/jmyounghoyos",
+    github: "https://github.com/Youngermaster/ST0256-NumAnalytic-Solver",
+    docs: "https://github.com/Youngermaster/ST0256-NumAnalytic-Solver",
   },
 }
